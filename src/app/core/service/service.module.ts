ce7bc5dc8effed2ec4ce8c81800cafa307d6e05f
Michael Ben Ali <michael.benali@gmail.com>
2020-03-02 08:39:17 +0100

    initial commit

---
+++ src/app/core/service/service.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ExampleService} from './example.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    ExampleService
+  ]
+})
+export class ServiceModule { }
